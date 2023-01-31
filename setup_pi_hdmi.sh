ad4f8311b6565b7825667c41c70f40cabc13c41e
cgauthier-unh <123567466+cgauthier-unh@users.noreply.github.com>
2023-01-31 08:07:21 -0500

    Add files via upload

---
+++ setup_pi_hdmi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Script to setup raspberrypi
+# Example how to run with 2 args for location name changes:
+# . setup_pi.sh lyr LYR
+
+# Update and upgrade system
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install libraries
+
+
+# Making ULF directory and installing codes
+mkdir ULF
+git clone https://github.com/cgauthier-unh/MIRL_HDMI
+mv MIRL_HDMI/* ULF/
+rm -r MIRL_ULF
+cd ULF
+sed -i "s/pi-unh-daq/pi-$1-daq/g" *
+sed -i "s/UNH/$2/g" *
+mkdir Data_Files
+
+# Make shell scripts executable and compile main_acq
+chmod 755 *.sh
+make
+
+# Moving files from ULF to proper locations
+sudo mv rc.local /etc/
+
