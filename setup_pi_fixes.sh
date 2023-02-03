06ba4c1fb69a1efb693d6af14895d5ec90cdbd25
cgauthier-unh <123567466+cgauthier-unh@users.noreply.github.com>
2023-02-03 15:39:08 -0500

    Update setup_pi_fixes.sh

--- setup_pi_fixes.sh
+++ setup_pi_fixes.sh
@@ -10,8 +10,8 @@ git clone https://github.com/cgauthier-unh/MIRL_ULF
 mv MIRL_ULF/* ULF/
 sudo rm -r MIRL_ULF
 cd ULF
-sed -i "s/pi-unh-daq/pi-$1-daq/g" *
-sed -i "s/UNH/$2/g" *
+sed -i "s/pi-unh-daq/pi-$1-daq/g" *.*
+sed -i "s/UNH/$2/g" *.*
 
 
 # Make shell scripts executable and compile main_acq
